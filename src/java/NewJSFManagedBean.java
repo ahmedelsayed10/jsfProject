7457a7f22679b3885527fe203498c82e243833f3
ahmed elhagrasi <micostar0000@gmail.com>
2020-03-10 03:39:27 +0200

    new edit

---
+++ src/java/NewJSFManagedBean.java
@@ -0,0 +1,50 @@
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+
+ 
+
+ 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+ 
+/**
+ *
+ * @author ahmed elhagrasi
+ */
+ @ManagedBean(name = "bean")
+ @SessionScoped
+public class NewJSFManagedBean {
+
+    private int id;
+    private String name;
+    /**
+     * Creates a new instance of NewJSFManagedBean
+     */
+    public NewJSFManagedBean() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    
+}
